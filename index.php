07a0599e234035fc1dadb0ec5fc34c18562c9eb9
Devjyot Singh Sidhu <devjyotsinhsidhu@gmail.com>
2021-03-20 00:40:26 +0530

    Initial Code Push

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="icon" href="Resources/logo.ico">
+    <title>Promodoro - The Productive Timer</title>
+    <link rel="stylesheet" href="css/master.css">
+    <link rel="stylesheet" href="css/index.css">
+  </head>
+  <body>
+    <?php include(__DIR__.'/left-div.php') ?>
+  </body>
+</html>
---
+++ left-div.php
@@ -0,0 +1,20 @@
+<div class="left-div">
+  <img src="Resources/Logo.png"></img>
+  <div class="nav-div">
+    <a href="#">
+      <div class="nav-subdiv">
+        Home
+      </div>
+    </a>
+    <a href="#">
+      <div class="nav-subdiv">
+        Timer
+      </div>
+    </a>
+    <a href="#">
+      <div class="nav-subdiv">
+        Statistics
+      </div>
+    </a>
+  </div>
+</div>
