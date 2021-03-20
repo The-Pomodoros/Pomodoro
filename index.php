f8d12e575940a168f45b87cd842e4efed7c8f6aa
Devjyot Singh Sidhu <devjyotsinhsidhu@gmail.com>
2021-03-20 10:31:48 +0530

    Corrected title typo

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
   <head>
     <meta charset="utf-8">
     <link rel="icon" href="Resources/logo.ico">
-    <title>Promodoro - The Productive Timer</title>
+    <title>Pomodoro - The Productive Timer</title>
     <link rel="stylesheet" href="css/master.css">
     <link rel="stylesheet" href="css/index.css">
   </head>
   <body>
     <?php include(__DIR__.'/left-div.php') ?>
     <div class="right-div">
-      
+
     </div>
   </body>
 </html>
